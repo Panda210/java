18b525eadae4ce93c002a90c4d595e783c30b6c0
xjl_210@126.com <law40252048>
2019-02-13 14:37:49 +0800

    first commit

---
+++ src/main/java/com/slife/java/demo1/RunApplication.java
@@ -0,0 +1,27 @@
+
+package com.slife.java.demo1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import app.myoss.cloud.web.spring.boot.BootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * 项目启动类
+ *
+ * @author Joyce.xu
+ * @since 2019年2月13日 下午2:18:21
+ */
+@Slf4j
+@SpringBootApplication
+public class RunApplication {
+    /**
+     * 项目启动类
+     *
+     * @param args 启动参数
+     */
+    public static void main(String[] args) {
+        BootApplication.run(log, RunApplication.class, args);
+    }
+}
---
+++ src/main/java/com/slife/java/demo1/biz/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * 类package-info的实现描述：Biz业务代码
+ *
+ * @author Joyce.xu
+ * @since 2019年2月13日 下午2:18:21
+ */
+package com.slife.java.demo1.biz;
+
